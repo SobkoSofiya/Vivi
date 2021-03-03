9014d8bd6df87de7a3f229dba9fd9235ffb71c66
SobkoSofiya <sobkosofiya23@gmail.com>
2021-03-03 06:44:57 +0300

    commit

--- Vivi/ViewModel.swift
+++ Vivi/ViewModel.swift
@@ -29,3 +29,46 @@ class ViewModel: ObservableObject {
         }
     }
 }
+
+
+func signIn(email: String, password: String, completionHandler: ((_ result: String,_ error: String) -> Void)? = nil) {
+            let url = "http://cinema.areas.su/auth/login"
+            let parameters: [String: String] = [
+                        "email": email,
+                        "password": password
+                    ]
+
+            AF.request(url, method: .post, parameters:parameters, encoder: JSONParameterEncoder.default).validate().responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    let token = json["token"].stringValue
+                    UserDefaults.standard.set(token, forKey: "token")
+                    completionHandler!(token, "Success")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    completionHandler!("", error.localizedDescription)
+                }
+                
+            }
+        }
+    
+    func getUser(completionHandler: ((_ result: String,_ error: String) -> Void)? = nil) {
+        let url = "http://cinema.areas.su/user"
+        let token = UserDefaults.standard.string(forKey: "token")!
+        let headers: HTTPHeaders = [
+                    "Authorization": "Bearer \(token)"
+                ]
+        AF.request(url, method: .get, headers: headers).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+            case .failure(let error):
+                print(error.localizedDescription)
+                completionHandler!("", error.localizedDescription)
+            }
+            
+        }
+        
+    }
